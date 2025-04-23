7e419196713d3d403d0a90118ccfead29e6f3a94
Zhikai <126306471+Evil-Genus@users.noreply.github.com>
2025-04-23 18:04:51 +0800

    Update lexer_test.go

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -32,3 +32,51 @@ func main() {
 		t.Log(token.String())
 	}
 }
+// 多层if-else嵌套测试
+func TestIFLexer(t *testing.T) {
+	testStr := `
+	if r == '/' {
+		if nextRune == '/' && nextRune != '*' {
+		} else if nextRune == '*' {
+			Println("")
+		} else {
+			Println("")
+		}
+	}`
+	l := lexer.NewLexer(strings.NewReader(testStr))
+	for {
+		token, err := l.NextToken()
+		if err != nil {
+			t.Fatalf("Error: %v", err)
+		}
+		if token.Type == lexer.EOF {
+			break
+		}
+		t.Log(token.String())
+	}
+}
+
+// switch语句测试
+func TestSwitchLexer(t *testing.T) {
+	testStr := `
+switch x {
+case 1:
+    fmt.Println("One")
+case 2:
+    fmt.Println("Two")
+default:
+    fmt.Println("Default")
+}
+`
+	l := lexer.NewLexer(strings.NewReader(testStr))
+	for {
+		token, err := l.NextToken()
+		if err != nil {
+			t.Fatalf("Error: %v", err)
+		}
+		if token.Type == lexer.EOF {
+			break
+		}
+		t.Log(token.String())
+	}
+}
